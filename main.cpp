3eadf66a9c015576538f54a7d47b0b7e8b34d987
Daniel Pfeifer <daniel@pfeifer-mail.de>
2011-03-23 15:16:50 +0100

    removing more unneeded files

--- main.cpp
+++ main.cpp
@@ -1,30 +1,23 @@
 //
 
-#include <deque>
-#include <string>
+#include <iostream>
+#include <boost/thread.hpp>
+#include <stream_process/preprocess.hpp>
 
-#include <stream_process/default_types.hpp>
-#include <stream_process/stream_manager.hpp>
-#include <stream_process/op_database.hpp>
-#include <stream_process/options_map.hpp>
-
-int main(int argc, char* argv_[])
+int main(int argc, char* argv[])
 {
-	const char** argv = const_cast<const char**> (argv_);
-
-	std::deque < std::string > modes;
-	modes.push_back("process");
-
-	stream_process::options_map omap;
-	size_t mode = omap.parse(modes, argc, argv);
-
 	std::cout << "stream processor 0.8 (c) jonas boesch, renato pajarola\n";
 	std::cout << "architecture: " << sizeof(void*) * 8 << "bit, precision: mixed.\n";
 	std::cout << std::endl;
 
-	stream_process::op_database odb;
-	stream_process::stream_manager<stream_process::sp_mixed_precision_type> sm;
-	odb.insert_ops(sm.get_op_manager());
-	sm.setup2(argc, argv, false);
-	return sm.process2();
+	stream_process::preprocessor::params ppp;
+	ppp.source_file = argc > 1 ? argv[1] : "bunny.ply";
+	ppp.result_file = "bunny.stream";
+	ppp.do_optimal_transform = true;
+
+	ppp.number_of_threads = boost::thread::hardware_concurrency();
+	if (ppp.number_of_threads < 1)
+		ppp.number_of_threads = 1;
+
+	stream_process::preprocessor pp(ppp);
 }
