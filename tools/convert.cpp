3b9ea33a9b49384da25dfa17fd7377fd2cc7ced3
Daniel Pfeifer <daniel@pfeifer-mail.de>
2011-06-30 21:04:08 +0200

    fixed

--- tools/convert.cpp
+++ tools/convert.cpp
@@ -1,41 +1,271 @@
 #include <string>
 #include <vector>
 
+#include <boost/bind.hpp>
 #include <boost/foreach.hpp>
-#include <boost/shared_ptr.hpp>
+#include <boost/range/algorithm/for_each.hpp>
 
 #include <junk/attribute_accessor.hpp>
 #include <junk/mapped_data_set.hpp>
 
-#include "ply/ply.h"
+#include "ply/config.hpp"
+#include "ply/ply.hpp"
+#include "ply/byte_order.hpp"
+#include "ply/io_operators.hpp"
+#include "ply/ply_parser.hpp"
 
-// hardcoded offset of zero
-junk::attribute_accessor<junk::vec3ui> get_indices(0);
-
-struct tmp_face
+struct vec3_t
 {
-	int* vertices;
-	uint8_t number_of_vertices;
+	float x, y, z;
 };
 
-typedef boost::shared_ptr<PlyFile> shared_ply;
-static inline shared_ply open_ply(const std::string& filename)
+struct color_t
 {
-	int nelems;
-	char** elem_names;
-	int file_type;
-	float version;
+	float r, g, b;
+};
 
-	return shared_ply(ply_open_for_reading((char*) filename.c_str(), &nelems,
-			&elem_names, &file_type, &version), ply_close);
+typedef unsigned int triangle_t[3];
+
+junk::attribute_accessor<vec3_t> get_position(0);
+junk::attribute_accessor<vec3_t> get_normal(12);
+junk::attribute_accessor<color_t> get_color(24);
+junk::attribute_accessor<triangle_t> get_indices(0);
+
+class acc_ply_size
+{
+public:
+	acc_ply_size(std::size_t& vertices, std::size_t& faces) :
+			vertices(vertices), faces(faces)
+	{
+		ply_parser.end_header_callback(end_header);
+		ply_parser.element_definition_callback(boost::bind(&acc_ply_size::element_definition, this, _1, _2));
+	}
+
+	void operator()(const std::string& filename)
+	{
+		ply_parser.parse(filename);
+	}
+
+private:
+	ply::ply_parser::element_callbacks_type element_definition(const std::string& element, std::size_t size)
+	{
+		if (element == "vertex")
+			vertices += size;
+
+		if (element == "face")
+			faces += size;
+
+		return ply::ply_parser::element_callbacks_type();
+	}
+
+	static bool end_header()
+	{
+		return false;
+	}
+
+private:
+	ply::ply_parser ply_parser;
+
+	std::size_t& vertices;
+	std::size_t& faces;
+};
+
+class read_ply_data
+{
+public:
+	read_ply_data(junk::mapped_data_set& junk, bool normal, bool color);
+
+	void operator()(const std::string& filename)
+	{
+		ply_parser.parse(filename);
+	}
+
+private:
+	ply::ply_parser::element_callbacks_type element_definition(const std::string& element, std::size_t count);
+
+	template<typename ScalarType>
+	std::tr1::function<void(ScalarType)>
+	scalar_property_definition(const std::string& element, const std::string& property);
+
+	template<typename SizeType, typename ScalarType>
+	std::tr1::tuple<std::tr1::function<void(SizeType)>, std::tr1::function<void(ScalarType)>, std::tr1::function<void()> >
+	list_property_definition(const std::string& element, const std::string& property);
+
+	void vertex_begin()
+	{
+	}
+
+	void vertex_x(ply::float32 val)
+	{
+		get_position(*vrtx_it).x = val;
+	}
+
+	void vertex_y(ply::float32 val)
+	{
+		get_position(*vrtx_it).y = val;
+	}
+
+	void vertex_z(ply::float32 val)
+	{
+		get_position(*vrtx_it).z = val;
+	}
+
+	void vertex_nx(ply::float32 val)
+	{
+		get_normal(*vrtx_it).x = val;
+	}
+
+	void vertex_ny(ply::float32 val)
+	{
+		get_normal(*vrtx_it).y = val;
+	}
+
+	void vertex_nz(ply::float32 val)
+	{
+		get_normal(*vrtx_it).z = val;
+	}
+
+	void vertex_r(ply::uint8 val)
+	{
+		get_color(*vrtx_it).r = val;
+	}
+
+	void vertex_g(ply::uint8 val)
+	{
+		get_color(*vrtx_it).g = val;
+	}
+
+	void vertex_b(ply::uint8 val)
+	{
+		get_color(*vrtx_it).b = val;
+	}
+
+	void vertex_end()
+	{
+		++vrtx_it;
+	}
+
+	void face_begin()
+	{
+	}
+
+	void face_vertex_indices_begin(ply::uint8 size)
+	{
+		assert(size == 3 && "non triangle face!");
+		p = 0;
+	}
+
+	void face_vertex_indices_element(ply::int32 vertex_index)
+	{
+		get_indices(*face_it)[p++] = vertex_index;
+	}
+
+	void face_vertex_indices_end()
+	{
+	}
+
+	void face_end()
+	{
+		++face_it;
+	}
+
+private:
+	ply::ply_parser ply_parser;
+
+	bool normal;
+	bool color;
+
+	int p;
+
+	junk::mapped_data_set::iterator vrtx_it;
+	junk::mapped_data_set::iterator face_it;
+};
+
+ply::ply_parser::element_callbacks_type read_ply_data::element_definition(const std::string& element_name, std::size_t count)
+{
+	if (element_name == "vertex")
+		return ply::ply_parser::element_callbacks_type(boost::bind(&read_ply_data::vertex_begin, this), boost::bind(&read_ply_data::vertex_end, this));
+
+	if (element_name == "face")
+		return ply::ply_parser::element_callbacks_type(boost::bind(&read_ply_data::face_begin, this), boost::bind(&read_ply_data::face_end, this));
+
+	return ply::ply_parser::element_callbacks_type(0, 0);
 }
 
-static inline int elem_count(const shared_ply& ply, const char* elem)
+template<>
+std::tr1::function<void(ply::float32)> read_ply_data::scalar_property_definition(const std::string& element, const std::string& property)
 {
-	int size;
-	int properties;
-	ply_get_element_description(ply.get(), (char*) elem, &size, &properties);
-	return size;
+	if (element != "vertex")
+		return 0;
+
+	if (property == "x")
+		return boost::bind(&read_ply_data::vertex_x, this, _1);
+
+	if (property == "y")
+		return boost::bind(&read_ply_data::vertex_y, this, _1);
+
+	if (property == "z")
+		return boost::bind(&read_ply_data::vertex_z, this, _1);
+
+	if (normal)
+	{
+		if (property == "nx")
+			return boost::bind(&read_ply_data::vertex_nx, this, _1);
+
+		if (property == "ny")
+			return boost::bind(&read_ply_data::vertex_ny, this, _1);
+
+		if (property == "nz")
+			return boost::bind(&read_ply_data::vertex_nz, this, _1);
+	}
+
+	return 0;
+}
+
+template<>
+std::tr1::function<void(ply::uint8)> read_ply_data::scalar_property_definition(const std::string& element, const std::string& property)
+{
+	if (element != "vertex")
+		return 0;
+
+	if (color)
+	{
+		if (property == "r" || property == "red")
+			return boost::bind(&read_ply_data::vertex_r, this, _1);
+
+		if (property == "g" || property == "green")
+			return boost::bind(&read_ply_data::vertex_g, this, _1);
+
+		if (property == "b" || property == "blue")
+			return boost::bind(&read_ply_data::vertex_b, this, _1);
+	}
+
+	return 0;
+}
+
+template<>
+std::tr1::tuple<std::tr1::function<void(ply::uint8)>, std::tr1::function<void(ply::int32)>, std::tr1::function<void()> > read_ply_data::list_property_definition(const std::string& element, const std::string& property)
+{
+	typedef std::tr1::tuple<std::tr1::function<void(ply::uint8)>, std::tr1::function<void(ply::int32)>, std::tr1::function<void()> > return_type;
+
+	if (element == "face" && property == "vertex_indices")
+		return return_type(boost::bind(&read_ply_data::face_vertex_indices_begin, this, _1), boost::bind(&read_ply_data::face_vertex_indices_element, this, _1), boost::bind(&read_ply_data::face_vertex_indices_end, this));
+
+	return return_type(0, 0, 0);
+}
+
+read_ply_data::read_ply_data(junk::mapped_data_set& junk, bool normal, bool color) :
+		normal(normal), color(color), vrtx_it(junk.vbegin()), face_it(junk.fbegin())
+{
+	ply_parser.element_definition_callback(boost::bind(&read_ply_data::element_definition, this, _1, _2));
+
+	ply::ply_parser::scalar_property_definition_callbacks_type scalar_property_definition_callbacks;
+	ply::at<ply::float32>(scalar_property_definition_callbacks) = boost::bind(&read_ply_data::scalar_property_definition<ply::float32>, this, _1, _2);
+	ply_parser.scalar_property_definition_callbacks(scalar_property_definition_callbacks);
+
+	ply::ply_parser::list_property_definition_callbacks_type list_property_definition_callbacks;
+	ply::at<ply::uint8, ply::int32>(list_property_definition_callbacks) = boost::bind(&read_ply_data::list_property_definition<ply::uint8, ply::int32>, this, _1, _2);
+	ply_parser.list_property_definition_callbacks(list_property_definition_callbacks);
 }
 
 void setup_header(junk::header& header, bool normal, bool color)
@@ -44,10 +274,10 @@ void setup_header(junk::header& header, bool normal, bool color)
 
 	create_attribute(vs, "position", junk::SP_FLOAT_32, 3);
 
-	if(normal)
+	if (normal)
 		create_attribute(vs, "normal", junk::SP_FLOAT_32, 3);
 
-	if(color)
+	if (color)
 		create_attribute(vs, "color", junk::SP_UINT_8, 3);
 
 	junk::compute_offsets(vs);
@@ -57,119 +287,13 @@ void setup_header(junk::header& header, bool normal, bool color)
 	junk::compute_offsets(fs);
 }
 
-static void setup_ply(const shared_ply& ply, junk::header& header, bool normal, bool color)
-{
-	const junk::element& vs = header.vertex();
-
-	const junk::attribute& attr = get_attribute(vs, "position");
-	PlyProperty pos_properties[] = {
-		{ "x", PLY_FLOAT, PLY_FLOAT, attr.offset + 0 * sizeof(float), 0, 0, 0, 0 },
-		{ "y", PLY_FLOAT, PLY_FLOAT, attr.offset + 1 * sizeof(float), 0, 0, 0, 0 },
-		{ "z", PLY_FLOAT, PLY_FLOAT, attr.offset + 2 * sizeof(float), 0, 0, 0, 0 },
-	};
-
-	for (int i = 0; i < 3; ++i)
-		ply_get_property(ply.get(), "vertex", &pos_properties[i]);
-
-	if (normal)
-	{
-		const junk::attribute& attr = get_attribute(vs, "normal");
-		PlyProperty pos_properties[] = {
-			{ "nx", PLY_FLOAT, PLY_FLOAT, attr.offset + 0 * sizeof(float), 0, 0, 0, 0 },
-			{ "ny", PLY_FLOAT, PLY_FLOAT, attr.offset + 1 * sizeof(float), 0, 0, 0, 0 },
-			{ "nz", PLY_FLOAT, PLY_FLOAT, attr.offset + 2 * sizeof(float), 0, 0, 0, 0 },
-		};
-
-		for (int i = 0; i < 3; ++i)
-			ply_get_property(ply.get(), "vertex", &pos_properties[i]);
-	}
-
-	if (color)
-	{
-		const junk::attribute& attr = get_attribute(vs, "color");
-		PlyProperty pos_properties[] = {
-			{ "red", PLY_UCHAR, PLY_UCHAR, attr.offset + 0 * sizeof(char), 0, 0, 0, 0 },
-			{ "green", PLY_UCHAR, PLY_UCHAR, attr.offset + 1 * sizeof(char), 0, 0, 0, 0 },
-			{ "blue", PLY_UCHAR, PLY_UCHAR, attr.offset + 2 * sizeof(char), 0, 0, 0, 0 },
-		};
-
-		for (int i = 0; i < 3; ++i)
-			ply_get_property(ply.get(), "vertex", &pos_properties[i]);
-	}
-
-	PlyProperty face_property = {
-		"vertex_indices", PLY_INT, PLY_INT, offsetof(tmp_face, vertices),
-		1, PLY_UCHAR, PLY_UCHAR, offsetof(tmp_face, number_of_vertices)
-		};
-
-	ply_get_property(ply.get(), "face", &face_property);
-}
-
-static inline void _read_vertex_data(const shared_ply& ply,
-		std::size_t num_vertices, junk::mapped_data_set::iterator& vit)
-{
-	while (num_vertices--)
-		ply_get_element(ply.get(), *(vit++));
-}
-
-static void _read_face_data(const shared_ply& ply, std::size_t num_faces,
-		std::size_t vetex_offset, junk::mapped_data_set::iterator& fit)
-{
-	tmp_face tmp;
-
-	while (num_faces--)
-	{
-		ply_get_element(ply.get(), &tmp);
-		if (tmp.number_of_vertices != 3)
-			throw std::runtime_error("found non-triangle face in ply.");
-
-		junk::vec3ui& indices = get_indices(*(fit++));
-
-		for (std::size_t i = 0; i < 3; ++i)
-			indices[i] = tmp.vertices[i] + vetex_offset;
-	}
-}
-
 void convert(const std::vector<std::string>& input, junk::mapped_data_set& junk, bool normal, bool color)
 {
 	junk::header& header = junk.get_header();
-	header.vertex().size = 35947;
-	header.face().size = 69451;
 
-//	BOOST_FOREACH(const std::string& filename, input)
-//	{
-//		shared_ply ply_file = open_ply(filename);
-//		header.vertex().size += elem_count(ply_file, "vertex");
-//		header.face().size += elem_count(ply_file, "face");
-//
-//		std::cout << filename << std::endl;
-//	}
+	acc_ply_size acc(header.vertex().size, header.face().size);
+	boost::range::for_each(input, acc);
 
 	junk._setup(true);
-	std::size_t vetex_offset = 0;
-
-	junk::mapped_data_set::iterator vit = junk.vbegin();
-	junk::mapped_data_set::iterator vit_end = junk.vend();
-
-	junk::mapped_data_set::iterator fit = junk.fbegin();
-	junk::mapped_data_set::iterator fit_end = junk.fend();
-
-	BOOST_FOREACH(const std::string& filename, input)
-	{
-		std::cout << filename << std::endl;
-
-		shared_ply ply_file = open_ply(filename);
-		setup_ply(ply_file, header, normal, color);
-
-		std::size_t num_vertices = elem_count(ply_file, "vertex");
-		std::size_t num_faces = elem_count(ply_file, "face");
-
-		_read_vertex_data(ply_file, num_vertices, vit);
-		_read_face_data(ply_file, num_faces, vetex_offset, fit);
-
-		vetex_offset += num_vertices;
-	}
-
-	assert(++vit == vit_end);
-	assert(++fit == fit_end);
+	boost::range::for_each(input, read_ply_data(junk, normal, color));
 }
