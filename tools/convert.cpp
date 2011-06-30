ac20b069eee8fb14c310e96a9c604f885336b4a3
Daniel Pfeifer <daniel@pfeifer-mail.de>
2011-06-30 16:46:10 +0200

    foo

--- tools/convert.cpp
+++ tools/convert.cpp
@@ -7,11 +7,9 @@
 #include <junk/attribute_accessor.hpp>
 #include <junk/mapped_data_set.hpp>
 
-#include "ply/config.hpp"
-#include "ply/ply.hpp"
-#include "ply/byte_order.hpp"
-#include "ply/io_operators.hpp"
-#include "ply/ply_parser.hpp"
+#include "ply/ply.h"
+
+#pragma GCC diagnostic ignored "-Wwrite-strings"
 
 void setup_header(junk::header& header, bool normal, bool color)
 {
